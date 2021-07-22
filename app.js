71eb245444c1e17d0eff6e5f71f5481431168a68
Rodrigo <rodasaraujo97@gmail.com>
2021-07-22 18:50:59 +0100

    trustify

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-// var bodyParser = require('body-parser');
+var https = require('follow-redirects').https;
+var fs = require('fs');
 // var nodemailer = require('nodemailer');
 var app = express();
 
@@ -10,25 +11,81 @@ app.get('/', function (req, res) {
   res.send(fs.readFileSync('index.html', 'utf8'));
 });
 
-const request = require('request');
+const name = req.body['name'];
+const email = req.body['email'];
+const subject = req.body['subject'];
+const message = req.body['message'];
 
-request.post(
-  process.env.TRUSTIFI_URL + '/api/i/v1/email',
-  {
-    headers: {
-      'x-trustifi-key': process.env.TRUSTIFI_KEY,
-      'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
-    },
-    json: {
-      recipients: [{ email: 'test@trustificorp.org' }],
-      title: 'Title',
-      html: 'Body',
-    },
+var options = {
+  method: 'POST',
+  hostname: 'be.trustifi.com',
+  path: '/api/i/v1/email/preview',
+  headers: {
+    'x-trustifi-key': process.env.TRUSTIFI_KEY,
+    'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
+    'Content-Type': 'application/json',
   },
-  (err, res, body) => {
-    console.log(body);
-  }
-);
+  maxRedirects: 20,
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on('end', function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on('error', function (error) {
+    console.error(error);
+  });
+});
+
+var postData = JSON.stringify({
+    recipients: [
+      {
+        email: 'rodrigo.araujo.9731@gmail.com',
+        name: 'Rodrigo Araujo',
+      },
+    ],
+    title: 'subject',
+    html: 'message',
+    methods: {
+      postmark: false,
+      secureSend: false,
+      encryptContent: false,
+      secureReply: false,
+    },
+  }(subject, message)),
+  ;
+
+req.write(postData);
+
+req.end();
+
+// const request = require('request');
+
+// request.post(
+//   process.env.TRUSTIFI_URL + '/api/i/v1/email',
+//   {
+//     headers: {
+//       'x-trustifi-key': process.env.TRUSTIFI_KEY,
+//       'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
+//     },
+//     json: {
+//       recipients: [{ email: 'test@trustificorp.org' }],
+//       title: 'Title',
+//       html: 'Body',
+//     },
+//   },
+//   (err, res, body) => {
+//     console.log(body);
+//   }
+// );
 
 // app.post('/', function (req, res) {
 //   var name = req.body['name'];
